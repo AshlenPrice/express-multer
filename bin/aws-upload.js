65c38075929203f865c3ebbd2156a78d308f5752
ashlenprice <ashlen.price@gmail.com>
2017-03-06 14:18:04 -0500

    Fixes filename and folder problem
    previous files were just getting saved in the root directory of our
    bucket. Solved this by adding today's date as a folder namer and
    prepending out file name with that. The nest thing we did via
    crypto.randomBytes is create a random string to save our file as on AWS.
    The last thing that we did was promisify both s3.upload and
    crypto.randomByres so both implement promises that are resolved or
    rejected if they work or not.

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -1,11 +1,12 @@
 'use strict';
 
 require('dotenv').load();
+const crypto = require('crypto');
 const mime = require('mime');
 const path = require('path');
-const AWS = require ('aws-sdk');
+const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
-const fs = require ('fs');
+const fs = require('fs');
 
 let file = {
   path: process.argv[2],
@@ -13,21 +14,42 @@ let file = {
 };
 let mimeType = mime.lookup(file.path);
 
-let ext =  path.extname(file.path);
+let ext = path.extname(file.path);
+let folder = (new Date()).toISOString().split('T')[0];
 
 // file.path is the same as process.argv[2]
 let stream = fs.createReadStream(file.path);
 
-// console.log("stream is", stream);
+new Promise((resolve, reject) => {
+    crypto.randomBytes(16, (error, buffer) => {
+      if (error) {
+        reject(error);
+      } else {
+        console.log("buffer is ", buffer.toString('hex'));
+        resolve(buffer.toString('hex'));
+      }
+    });
+  }).then((filename) => {
+    let params = {
+      ACL: 'public-read',
+      ContentType: mimeType,
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: `${folder}/${filename}${ext}`,
+      Body: stream
+    };
 
-let params = {
-  ACL: 'public-read',
-  ContentType: mimeType,
-  Bucket: process.env.AWS_S3_BUCKET_NAME,
-  Key: `${file.title}${ext}`,
-  Body:  stream
-};
 
-s3.upload(params, function (err, data){
-  console.log(err, data);
-});
+    return new Promise((resolve, reject) => {
+      s3.upload(params, function(error, data) {
+        if (error) {
+          // console.log(error);
+          reject(error);
+        } else {
+          // console.log(data);
+          resolve(data);
+        }
+      });
+    });
+  })
+  .then(console.log)
+  .catch(console.error);
